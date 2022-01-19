dcbbbd1a6634f13fc353cd8a4df3fa69f82c7474
dilli babu <dillibabukadati777@gmail.com>
2022-01-19 08:24:46 +0530

    added side-nav-bar module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,18 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {SideMenuBarComponent} from './components/side-menu-bar/side-menu-bar.component';
 import {HeaderComponent} from './components/header/header.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
-import {SideBarMenuItemComponent} from './components/side-bar-menu-item/side-bar-menu-item.component';
-import {SideBarMenuDeviderComponent} from './components/side-bar-menu-devider/side-bar-menu-devider.component';
 import {IconsModule} from "./shared/icons.module";
 import {HttpClientModule} from "@angular/common/http";
-import { SideBarMenuGroupComponent } from './components/side-bar-menu-group/side-bar-menu-group.component';
-import { SideBarMenuCollapsableComponent } from './components/side-bar-menu-collapsable/side-bar-menu-collapsable.component';
+import {SideNavBarModule} from "./components/ui/side-nav-bar/side-nav-bar.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    SideMenuBarComponent,
     HeaderComponent,
-    SideBarMenuItemComponent,
-    SideBarMenuDeviderComponent,
-    SideBarMenuGroupComponent,
-    SideBarMenuCollapsableComponent
   ],
   imports: [
     IconsModule,
@@ -33,6 +24,7 @@ import { SideBarMenuCollapsableComponent } from './components/side-bar-menu-coll
     MatButtonModule,
     MatIconModule,
     BrowserAnimationsModule,
+    SideNavBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
